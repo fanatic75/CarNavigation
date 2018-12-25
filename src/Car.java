48a7fb3813979838d6e1925af9eed9ed5737a0a5
Banga <prateekbanga12@gmail.com>
2018-12-25 13:48:18 +0530

    removed un necessary things

--- src/Car.java
+++ src/Car.java
@@ -2,10 +2,11 @@ import java.util.*;
 import java.lang.*;
 public class Car {
     private String route[];
-    private Scanner scan= new Scanner(System.in);
-    private int startingPosition[]=new int[2];
-    private int currentPosition[]= new int[2];
-    protected void takeInput(){
+    private Scanner scan = new Scanner(System.in);
+    private int startingPosition[] = new int[2];
+    private int currentPosition[] = new int[2];
+
+    protected void takeInput() {
         System.out.println("Enter the starting position");
         takeCurrentPosition();
         System.out.println("Enter the way to go in directions such as N10 or S20 or E10 or W5");
@@ -14,30 +15,32 @@ public class Car {
 
     }
 
-    protected void moveThroughRoute(){
-        for(int i=0;i<route.length;i++){
+    protected void moveThroughRoute() {
+        for (int i = 0; i < route.length; i++) {
             try {
                 int temp = Integer.parseInt(route[i].substring(1));
-            String switchCase=route[i].substring(0,1);
-            switch(switchCase){
-                case "N":
-                    currentPosition[1]=currentPosition[1]+temp;
-                    break;
-                case "S":
-                    currentPosition[1]=currentPosition[1]-temp;
-                    break;
-                case "E":
-                    currentPosition[0]=currentPosition[0]+temp;
-                    break;
-                case "W":
-                    currentPosition[0]=currentPosition[0]-temp;
-                    break;
-                default:
-                    System.out.println("Route values are not proper");
-                    System.exit(1);
+                String switchCase = route[i].substring(0, 1).toLowerCase();
+                System.out.println(currentPosition[1]);
+                System.out.println(temp);
+                switch (switchCase) {
+                    case "n":
+                        currentPosition[1] = currentPosition[1] + temp;
+                        break;
+                    case "s":
+                        currentPosition[1] = currentPosition[1] - temp;
+                        break;
+                    case "e":
+                        currentPosition[0] = currentPosition[0] + temp;
+                        break;
+                    case "w":
+                        currentPosition[0] = currentPosition[0] - temp;
+                        break;
+                    default:
+                        System.out.println("Route values are not proper");
+                        System.exit(1);
 
-            }
-            }catch(NumberFormatException e){
+                }
+            } catch (NumberFormatException e) {
                 System.out.println("Values are not proper");
                 System.exit(1);
             }
@@ -45,38 +48,38 @@ public class Car {
         System.out.println(Arrays.toString(currentPosition));
     }
 
-    protected double locationDisplacement(){
-        double displacement=Math.sqrt(((currentPosition[0]-startingPosition[0])*(currentPosition[0]-startingPosition[0]))+((currentPosition[1]-startingPosition[1])*(currentPosition[1]-startingPosition[1])));
+    protected double locationDisplacement() {
+        double displacement = Math.sqrt(((currentPosition[0] - startingPosition[0]) * (currentPosition[0] - startingPosition[0])) + ((currentPosition[1] - startingPosition[1]) * (currentPosition[1] - startingPosition[1])));
         return displacement;
     }
 
-    private void takeCurrentPosition(){
-        boolean done=false;
-            do{
-                if(scan.hasNextInt()){
-                 currentPosition[0] = scan.nextInt();
-                 scan.nextLine();
-                 startingPosition[0]=currentPosition[0];
-                 done = true;
-                 }else{
+    private void takeCurrentPosition() {
+
+            boolean done = false;
+            do {
+                if (scan.hasNextInt()) {
+                    currentPosition[0] = scan.nextInt();
+                    scan.nextLine();
+                    startingPosition[0] = currentPosition[0];
+                    done = true;
+                } else {
                     scan.nextLine();
                     System.out.println("Enter a valid Integer value");
-                    }
-            }while(!done);
-
-        done=false;
-        do{
-
-            if(scan.hasNextInt()){
+                }
+            } while (!done);
+                done = false;
+        do {
+            if (scan.hasNextInt()) {
                 currentPosition[1] = scan.nextInt();
                 scan.nextLine();
-                startingPosition[1]=currentPosition[1];
+                startingPosition[1] = currentPosition[1];
                 done = true;
-            }else{
+            } else {
                 scan.nextLine();
                 System.out.println("Enter a valid Integer value");
             }
-        }while(!done);
+        } while (!done);
+
     }
 
     private void takeRoute(){
--- src/CarNavigation.java
+++ src/CarNavigation.java
@@ -1,8 +1,6 @@
 import java.util.*;
 
 public class CarNavigation{
-    Scanner scan = new Scanner(System.in);
-    private  String[] inputValues;
    public static void main(String ...args){
        Car car = new Car();
        car.takeInput();
